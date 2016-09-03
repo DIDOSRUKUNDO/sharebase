014d91f53641b78e7d4e226e9c58af518604c302
GROUPSHARE <didorukundo@gmail.com>
2016-09-03 10:37:37 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<title>Groushare Home page</title>
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+</head>
+<body>
+<div id="wrapper">
+<div id="bg">
+
+<div id="nav">
+<div class="logo">
+<h3>Groupshare</h3>
+</div>
+<ul>
+<li><a href="#"><img src="images/home-5-32.png"></a></li>
+<li><a href="#">Index</a></li>
+<li><a href="#">Primary</a></li>
+<li><a href="#">O-Level</a></li>
+<li><a href="#">A-Level</a></li>
+<li><a href="#">University</a></li>
+<li><a href="#">Job-Papers</a></li>
+<li><a href="#">About us</a></li>
+</ul>
+</div>
+<div id="header">
+<h2>Welcome to Groupshare - Over 1000 exams with over 3 million views for free</h2>
+<form>
+<input type="text"name="email" placeholder="Search...">
+<button>Search</button>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
